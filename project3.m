bd0a5afa9cc7d74144f786fd5f1f5f7d0f0faffa
Buridi Aditya <buridiaditya@gmail.com>
2018-10-31 14:56:25 +0530

    working code for calculation jointprobability of stiulus

--- project3.m
+++ project3.m
@@ -107,15 +107,15 @@ tic
 costs = [0,0.001,0.01,1,10,100];
 s = RandStream('mlfg6331_64');
 
-processedspikes = cell(4,50,201);
+processedspikes = cell(4,50,200);
 
 parfor i=1:4
-    tempCell = cell(50,201);
+    tempCell = cell(50,200);
     for j=1:50
         for k=All_Spike_Times{i,j}
-            index = int64(k*1000/100)+1;
-            spiketrain = tempCell{j,index};
-            tempCell{j,index} = [spiketrain k];
+            ind = ceil(k*1000/100);
+            spiketrain = tempCell{j,ind};
+            tempCell{j,ind} = [spiketrain k];
         end        
     end
     processedspikes(i,:,:) = tempCell;
@@ -123,50 +123,54 @@ end
 
 save( 'samplespikes.mat', 'processedspikes');
 toc
-%%
-disp(processedspikes{1,1,1}(1));
-processedspi = load('samplespikes.mat');
+
 %%
 tic
-confusionMatrixTotal = zeros(100,4,6,8,8); 
+confusionMatrixTotal = zeros(100,4,8,8,6); 
 
-processedspikes = processedspi.processedspikes;
-%parfor i=1:100
+parfor i=1:100
+    processedspi = load('samplespikes.mat');
+    processedspikes = processedspi.processedspikes;
+    
     y = datasample(s,1:200,8,'Replace',false);  
-    confusionMatrix = zeros(4,6,8,8);
-    comparitions = zeros(400,400,6);
+    
+    confusionMatrix = zeros(4,8,8,6);
+    comparitions = ones(400,400,6)*Inf;
+    
     for j=1:4
-        perNeuron = processedspikes(j,1:end,1:end);
-        for k=1:8
-            for l=1:50
-                ST1 = perNeuron{1,l,y(k)};
-                for m=1:8 % compare with which 
-                    for n=1:50
-                        if ~(k==m && l==n)
-                            value = spkd_qpara(ST1,perNeuron{1,n,y(m)},costs);                
-                            comparitions((k-1)*50+l,(m-1)*50+n,:) = value;
-                            %comparitions((m-1)*50+n,(k-1)*50+l,:) = value; 
+        perNeuron = processedspikes(j,:,:);
+        for k=1:400
+                in1 = ceil(k/50);
+                ST1 = perNeuron{1,in1,y(in1)};
+                for m=1:400 % compare with which                     
+                        if ~(k==m) 
+                            if comparitions(k,m,:) == Inf
+                                in2 = ceil(m/50);
+                                value = spkd_qpara(ST1,perNeuron{1,in2,y(in2)},costs);                
+                                comparitions(k,m,:) = value;
+                                comparitions(m,k,:) = value;
+                            end
+                            %comparitions((m-1)*50+n,(k-1)*50+l,:) = value;                                                 
                         end
-                        if (k==m && l==n)
-                            comparitions((k-1)*50+l,(m-1)*50+n,:) = 100000;
-                        end
-                    end
                 end
-                [argvalue, index] = min(comparitions((k-1)*50+l,:,:),[],2);
-                index=ceil(index/50);
+                [argvalue, index] = min(reshape(comparitions(k,:,:),[400,6]),[],1);
+                index=ceil(index/50);                
+                %confusionMatrix(j,in1,index(1,1,:),[1,2,3,4,5,6]) = confusionMatrix(j,in1,index(1,1,:),[1,2,3,4,5,6]) + 1;
                 for o=1:length(index)
-                    confusionMatrix(j,o,k,index(1,1,o)) = confusionMatrix(j,o,k,index(1,1,o)) + 1;
+                    confusionMatrix(j,in1,index(o),o) = confusionMatrix(j,in1,index(o),o) + 1;
                 end
-            end           
+                       
         end
     end
-    confusionMatrixTotal(1,:,:,:,:) = confusionMatrix/50;
-%end
+    confusionMatrixTotal(i,:,:,:,:) = confusionMatrix/50;
+end
 toc
-%%
+%% Calculate mutual information
 
-arr = confusionMatrixTotal(1,1,1,1);
-disp(arr)
+
+
+
+disp(reshape(confusionMatrixTotal(1,1,:,:,1),[8,8]))
 
 %%
 function d=spkd_qpara(tli,tlj,costs)
@@ -202,4 +206,106 @@ function d=spkd_qpara(tli,tlj,costs)
         end
     end
     d=scr(:,nspi+1,nspj+1);
+end
+
+%% 
+
+function [clast,rmax,c]=spkdallq_recur(sa,sb)
+% function [clast,rmax,c]=spkdallqk_recur(sa,sb) does the recursion for the DP
+% algorithm for spike time distance.  It uses the method of spkdallq, but
+% reformats and produces arguments to be parallel to spkdallqk_recur.
+%
+% sa: vector of spike times for first spike train
+% sb: vector of spike times for second spike train
+% c: array of critical lengthts of linkages. size(c)=[1+length(sa) 1+length(sb) 1+rmax]
+% rmax: maximum number of linkages (=min(length(sa),length(sb))
+% clast: vector of critical lengths for full spike trains, necessary for calculating all
+%    distances, which is done in spkdallq_dist, via spkdallq_final.
+%
+%  Copyright (c) by Jonathan Victor.
+%
+%    See also SPKD, SPKDALLQ, SPKDALLQK_RECUR.
+%
+tli=sa; %reformat the input arguments
+tlj=sb;
+nspi=length(tli);
+nspj=length(tlj);
+nij=min(nspi,nspj);
+lc=repmat(NaN,[nspi+1 nspj+1 nij]); % assume no length of movement
+if (nij>0)
+%
+%     best linkage length is a recursion based on linkages for shorter trains
+%
+	for i=2:nspi+1
+   	for j=2:nspj+1
+         td=abs(tli(i-1)-tlj(j-1));
+         li=squeeze(lc(i-1,j,:));
+         lj=squeeze(lc(i,j-1,:));
+         lij=td+[0;squeeze(lc(i-1,j-1,1:nij-1))];
+         lc(i,j,:)=min([li,lj,lij],[],2);
+   	end
+	end
+end
+rmax=nij;
+c=cat(3,zeros(nspi+1,nspj+1),lc);
+clast=squeeze(c(end,end,:));
+return
+end
+
+function dists=spkdallq_final(qlist,sa,sb,clast,rmax)
+% dists=spkdallq_final(qlist,sa,sb,clast,rmax) does the 
+% final portion of parallel DP spike tima algorithm, extended to simultaneous
+%  calculation for all values of q.
+%
+%    dists: a column vector of the distances
+%    qlist: a column vector of values of q. qklist(:,1): q-values
+%    sa, sb: spike times on the two spike trains
+%    clast, rmax: calculated by spkdallq_recur
+%
+% See spkdallqk.doc.
+%
+%  Copyright (c) by Jonathan Victor.
+%
+%    See also SPKDALLQ_RECUR, SPKDALLQ_DIST, SPKD, SPKDALLQ.
+%
+%
+na=length(sa);
+nb=length(sb);
+nq=length(qlist);
+qlist=reshape(qlist,[nq 1]);
+%
+%make column vector of na+nb-2r, indicating costs of deletions and insertions
+%
+nanbrs=(na+nb)*ones(1+rmax,1)-2*[0:rmax]';
+%
+% find the best strategy (all r (matched links) and all s (mismatched links)
+%
+clast=reshape(clast,[1+rmax 1]);
+for iq=1:nq
+   posscosts=qlist(iq,1)*clast+nanbrs;
+   dists(iq,1)=min(min(posscosts));
+end
+return
+end
+
+function [dists,clast,rmax]=spkdallq_dist(qlist,sa,sb)
+% function [dists,clast,rmax]=spkdallq_dist(qlist,sa,sb) does the 
+% recursion and final portion of the parallel DP single unit algorithm, extended to simultaneous
+%  calculation for all values of q.
+%
+%    qlist: a column vector of values of q. qklist(:,1): q-values.
+%    sa, sb: spike times on the two spike trains
+%    clast, rmax: calculated by spkdallq_recur.
+%
+% See spkdallqk.doc.
+%
+%  Copyright (c) by Jonathan Victor.
+%
+%    See also SPKDALLQ_RECUR, SPKDALLQ_DIST.
+%
+%do the recursion
+[clast,rmax,c]=spkdallq_recur(sa,sb);
+%do the final stage
+dists=spkdallq_final(qlist,sa,sb,clast,rmax);
+return
 end
